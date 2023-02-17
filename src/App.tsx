a88c48a1e718fccc9408878e140755b8ae6ebfcd
Uğur Yıldız <yldz2534@gmail.com>
2023-02-17 07:59:28 +0300

    redux import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers'; 
 
 class App extends Component {
   render() {
-    return (
-      <View>
-        <Text>Book List</Text>
-      </View>
+    return ( 
+      <Provider store={createStore(reducers)}>
+        <View>
+        </View>
+      </Provider>
     )
   }
 }
 
-export default App;
+export default App; 
---
+++ src/reducers/index.tsx
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({
+    books: () => []
+})
+// export default combineReducers({
+//     books: () => []
+// })
+
+export default reducers;
